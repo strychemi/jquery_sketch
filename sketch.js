214067da01f3f5a88bd74160bc45dc2f7946e74d
Andrew Baik <and.baik@gmail.com>
2015-11-12 18:37:41 -0500

    finished reset resolution feature

--- sketch.js
+++ sketch.js
@@ -1,7 +1,14 @@
 $(document).ready(function() {
-    initialize_board();
+    initialize_board(16);
 
-  
+    $('.square').hover(function() {
+        $(this).css("background-color", "grey");
+
+    });
+
+    document.getElementById("btn-1").onclick = function() {
+        reset_resolution()
+    };
 });
 
 function initialize_board() {
@@ -10,4 +17,21 @@ function initialize_board() {
     }
     $(".square").height("200px");
     $(".square").width("200px");
+}
+
+function reset_resolution() {
+    var squares = prompt("How many squares per side do you want for the grid?");
+
+    $(".container").empty();
+
+    var square_length = (800/squares);
+    for (var n = 0; n < squares * squares; n++) {
+        $('.container').append("<div class=square></div>");
+    }
+    $(".square").height(square_length);
+    $(".square").width(square_length);
+
+    $(".square").hover(function() {
+        $(this).css("background-color", "grey");
+    });
 }
